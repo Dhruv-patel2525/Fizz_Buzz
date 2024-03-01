61eae169481712be5932f436b45d472a6a3be037
neha-code19 <53231876+neha-code19@users.noreply.github.com>
2024-02-29 21:39:05 -0500

    Passed for multiple of 3 and multiple of 5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static String FizzBuzz(int n)
     {
+        if(n%15 == 0)
+            return "FizzBuzz";
         if(n%3 == 0)
             return "Fizz" ;
         if(n%5 == 0)
